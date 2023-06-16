89b93ceb4f66e5159ff283efd2c12dd00ca24efd
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-06-16 10:02:18 +0200

    install and configured daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
     <p className='m-12'>Lorem ips</p>
+    <button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
     </div>
   
   );
