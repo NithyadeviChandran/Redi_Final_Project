b1c6a3cf92da66a791e62806fd58d3587e51c417
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-06-16 11:06:32 +0200

    added the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="App min-h-screen">
       <Nav/>
     <main>main</main>
-    <footer>footer</footer>
+    <Footer/>
     </div>
 
   );
