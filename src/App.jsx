a7a31aa9df2e8a3cfb09859da2d818895b8670c0
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-06-19 13:27:27 +0200

    added the Home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Main from './components/Main';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App min-h-screen">
       <Nav/>
-    <main>main</main>
-    <Footer/>
+      <Home/>
+      {/* <Main/> */}
+      <Footer/>
     </div>
 
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <div className="navbar bg-neutral text-neutral-content">
     <div className="navbar-start">
-      <div className="dropdown">
+      {/* <div className="dropdown">
         <label tabIndex={0} className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </label>
@@ -19,10 +19,10 @@ function Nav() {
           </li>
           <li><a>Item 3</a></li>
         </ul>
-      </div>
-      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div> */}
+      <a className="btn btn-neutral normal-case text-xl">MemoryLane<ion-icon style={{fontSize:'1.3em'}} name="pulse-outline"></ion-icon></a>
     </div>
-    <div className="navbar-center hidden lg:flex">
+    {/* <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
         <li><a>Item 1</a></li>
         <li tabIndex={0}>
@@ -36,10 +36,10 @@ function Nav() {
         </li>
         <li><a>Item 3</a></li>
       </ul>
-    </div>
-    <div className="navbar-end">
+    </div> */}
+    {/* <div className="navbar-end">
       <a className="btn">Button</a>
-    </div>
+    </div> */}
   </div>
   )
 }
