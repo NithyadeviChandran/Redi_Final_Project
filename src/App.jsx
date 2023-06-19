367a21d7198ca824624de18f7fe7b5d1d0d5a6b5
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-06-19 13:34:11 +0200

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import Main from './components/Main';
 import Nav from './components/Nav';
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App min-h-screen">
       <Nav/>
-      <Home/>
-      {/* <Main/> */}
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='calendar' element={<Main/> }/>
+      </Routes>
       <Footer/>
     </div>
 
