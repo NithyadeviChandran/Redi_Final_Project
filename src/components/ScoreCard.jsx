91d229b1ea61020b9434abfa72c831a13be4d7b4
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-06-25 13:18:06 +0200

    updated the scorecard-chart appearing

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import {useEffect} from 'react'
 import {  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
 
-import datajson from '../data.json'
-
 function ScoreCard({ scoreCard,child1,score,setScore}) {
 
  
@@ -21,17 +19,19 @@ function ScoreCard({ scoreCard,child1,score,setScore}) {
   
   return (
     <>
-    <div className="App min-h-screen flex flex-col justify-normal items-center gap-10">
       <h1>Scorecard</h1>
-    <BarChart width={350} height={300} data={score.subject}>
+    {score && score.subject.map((i)=>  
+            <BarChart width={350} height={300} data={score.subject}>
               <CartesianGrid strokeDasharray="3 3" />
               <Tooltip />
               <Legend />
-            <Bar dataKey="value" fill="cornflowerblue" />
-            <XAxis dataKey="name" />
+            <Bar dataKey="value" fill="blue" />
+            <XAxis dataKey="name"/>
             <YAxis />
     </BarChart>
-    </div>
+    
+    )}
+
 </>
   )
 }
