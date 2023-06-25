1d103e0787780b1260d064cc95579e84892f319d
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-06-25 15:05:40 +0200

    chart component completed

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,39 +1,47 @@
-import React from 'react'
-import {useEffect} from 'react'
-import {  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
+import React from "react";
+import { useEffect } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  LineChart,
+  Line,
+} from "recharts";
 
-function ScoreCard({ scoreCard,child1,score,setScore}) {
+function ScoreCard({ scoreCard, child1, score, setScore }) {
+  let result = "";
+  if (child1) {
+    result = scoreCard && scoreCard.filter((i) => i.childId == child1.id);
+    console.log(result);
+  }
 
- 
-    let result = "";
-    if (child1) {
-      result = scoreCard && scoreCard.filter((i) => i.childId == child1.id);
-      console.log(result)
+  useEffect(() => {
+    if (result) {
+      setScore(result[0]);
     }
-  
-    useEffect(() => {
-      if (result) {
-        setScore(result[0]);
-      }
-    }, []);
-  
+  }, []);
+
   return (
     <>
-      <h1>Scorecard</h1>
-    {score && score.subject.map((i)=>  
-            <BarChart width={350} height={300} data={score.subject}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip />
-              <Legend />
+      <div className="w-72">
+        <h1 className="text-center">Scorecard</h1>
+        {score && (
+          <BarChart width={350} height={300} data={score.subject}>
+            {/* <CartesianGrid strokeDasharray="1 1" /> */}
+            <Tooltip />
+            <Legend />
             <Bar dataKey="value" fill="blue" />
-            <XAxis dataKey="name"/>
+            <XAxis dataKey="name" />
             <YAxis />
-    </BarChart>
-    
-    )}
-
-</>
-  )
+          </BarChart>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default ScoreCard
+export default ScoreCard;
