ea6ece3cea3b0e5c40ed465accdf89bc44d4dfa7
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-06-30 10:26:41 +0200

    register button size changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -72,7 +72,7 @@ function Home({ users, setUser }) {
                   </button>
                   <Link to="/register">
                     <button
-                      className="btn btn-primary w-full max-w-xs md:btn-primary btn-sm w-14 mx-28"
+                      className="btn btn-primary max-w-xs md:btn-primary btn-sm w-28 mx-28"
                       type="submit"
                     >
                       Register
